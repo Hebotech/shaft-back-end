e60c1607fef9bb463e9d793f99751a87b7e04181
IrfDev <irving.dev@icloud.om>
2020-03-30 17:38:38 -0600

    Update hubspotCompanies.js

--- src/router/hubspotCompanies.js
+++ src/router/hubspotCompanies.js
@@ -4,7 +4,7 @@ const hubspot = require('../lib/hubspot')
 
 router.get('/', async(req, res) => {
     let opts = {
-        properties: ['name', 'website', 'country'],
+        properties: ['name', 'website', 'country', 'clics_de_hebotech'],
     }
     try {
         const allCompanies = await hubspot.companies.get(opts)
@@ -12,13 +12,31 @@ router.get('/', async(req, res) => {
             success: true,
             message: 'All companies',
             data: allCompanies
-
         })
     } catch (error) {
         res.json({
             success: false,
             message: 'No se obtuvieron las compañías',
             error: error
+        })
+    }
+})
+
+router.put('/:id', async(req, res) => {
+    try {
+        const { id } = req.params
+        const body = req.body
+        const companyUpdate = await hubspot.companies.update(id, body)
+        res.json({
+            success: true,
+            message: `Company ${id} update`,
+            data: companyUpdate
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            message: 'Something went wrong',
+            error: error.message
         })
     }
 })
